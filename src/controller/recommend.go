d1fe6160931689ae951f1f6bf875ac3b5fd8775d
kanakanho <kahoshibakichi@gmail.com>
2024-06-27 20:01:30 +0900

    fix: モックとして動作するように

--- src/controller/recommend.go
+++ src/controller/recommend.go
@@ -16,37 +16,14 @@ func RecommendContents(c *gin.Context) {
 	body := buf.String()
 
 	// リクエストボディをJSONに変換
-	var requestBody map[string]interface{}
+	var requestBody common.RecommendContentsRequest
 	if err := json.Unmarshal([]byte(body), &requestBody); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	// リクエストボディの中身を確認
-	if _, ok := requestBody["userId"]; !ok {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "userId is required"})
-		return
-	}
-
-	if _, ok := requestBody["x"]; !ok {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "absoluteAddress.x is required"})
-		return
-	}
-
-	if _, ok := requestBody["y"]; !ok {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "absoluteAddress.y is required"})
-		return
-	}
-
-	if _, ok := requestBody["z"]; !ok {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "absoluteAddress.z is required"})
-		return
-	}
-
 	response := common.RecommendContentsResponse{
-		ContentIds: []string{"01F8VYXK67BGC1F9RP1E4S9YK1", "01F8VYXK67BGC1F9RP1E4S9YK2", "01F8VYXK67BGC1F9RP1E4S9YK3"},
+		ContentIds: []string{"22b86d69-9340-4abf-a143-3976877035bf"},
 	}
-	c.JSON(201, gin.H{
-		"contentIds": response.ContentIds,
-	})
+	c.JSON(http.StatusOK, response)
 }
